31d5efa40f2741bae506b59acbda75e1e7f450df
Girish022 <34120719+Girish022@users.noreply.github.com>
2022-11-12 17:23:23 +0530

    Update StandardGradeBook .cs

--- GradeBook/GradeBooks/StandardGradeBook .cs
+++ GradeBook/GradeBooks/StandardGradeBook .cs
@@ -1,14 +1,10 @@
-using System;
-using System.Collections.Generic;
-using GradeBook.Enums;
-
 namespace GradeBook.GradeBooks
 {
     public class StandardGradeBook : BaseGradeBook
     {
         public StandardGradeBook(string name) : base(name)
         {
-            Type = GradeBookType.Standard;
+            Name = name;
         }
     }
 }
