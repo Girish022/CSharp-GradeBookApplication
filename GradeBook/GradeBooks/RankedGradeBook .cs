150905aeb6e41248b752799c9bfdb882710f1c4a
Girish022 <34120719+Girish022@users.noreply.github.com>
2022-11-12 11:35:39 +0000

    Add files via upload

---
+++ GradeBook/GradeBooks/RankedGradeBook .cs
@@ -0,0 +1,11 @@
+﻿namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            Name = name;
+            Students = new List<Student>();
+        }
+    }
+}
